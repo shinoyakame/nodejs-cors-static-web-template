89cb26412a0654cd2d7d700c0fa6e796338968fc
shinoyakame <shinoyakame02@gmail.com>
2021-01-15 23:09:04 +0700

    project init

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+var http = require('http');
+var fs = require('fs');
+const cors = require('cors');
+const app = express();
+var corsOptions = {
+    origin: 'http://localhost:8080',
+    optionsSuccessStatus: 200 // For legacy browser support
+}
+app.use(cors(corsOptions));
+app.use(express.static('public'))
+
+app.listen(8080, ()=>{
+    console.log('server start...');
+});
